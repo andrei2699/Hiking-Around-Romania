11e3aed02041bbf181f069b8837fefa20467bb50
Andrei Timar <atimar999@gmail.com>
2020-10-26 14:25:45 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // for firestore
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBESVCP0HIzFYKbVvpP7NzomeEA79Ed_LU",
+    authDomain: "hiking-around-romania.firebaseapp.com",
+    databaseURL: "https://hiking-around-romania.firebaseio.com",
+    projectId: "hiking-around-romania",
+    storageBucket: "hiking-around-romania.appspot.com",
+    messagingSenderId: "404507694994",
+    appId: "1:404507694994:web:63646d7f5d0b93aefe934b",
+    measurementId: "G-4EBHJZPK99"
+  }
 };
 
 /*
