620f114f865df8fc72406965425dc341aac982fb
Andrei Timar <atimar999@gmail.com>
2020-11-21 17:29:48 +0200

    added update event

--- src/app/events/event.service.ts
+++ src/app/events/event.service.ts
@@ -25,31 +25,47 @@ export class EventService {
       .uploadPhotoWithNameVariation('eventPhotos', organizerProfile.eventId, file, nameVariation);
   }
 
-  createEvent(eventDetails: EventDetails) {
-    const uploadData = {
-      accomodation: eventDetails.accomodation,
-      accomodationPrice: eventDetails.accomodationPrice,
-      description: eventDetails.eventDescription,
-      eventName: eventDetails.eventName,
-      eventMainPhotoUrl: eventDetails.eventMainPhotoUrl,
+  createEmptyEvent(organizerId, organizerName) {
 
-      eventPhotosUrl: eventDetails.eventPhotosUrl,
+    return this._firestore.collection('events').add({
+      accomodation: "",
+      accomodationPrice: 0,
+      description: "",
+      eventName: "",
+      eventMainPhotoUrl: "",
+      eventPhotosUrl: [],
+      eventPrice: 0,
+      mapLat: 0,
+      mapLng: 0,
+      organizerId: organizerId,
+      organizerName: organizerName,
+      transport: "",
+      transportPrice: "",
+    });
+  }
 
-      eventPrice: eventDetails.eventPrice,
-      mapLat: eventDetails.mapCenter.lat,
-      mapLng: eventDetails.mapCenter.lng,
-
-      organizerId: eventDetails.organizerId,
-      organizerName: eventDetails.organizerName,
-
-      transport: eventDetails.transport,
-      transportPrice: eventDetails.transportPrice,
-    };
-
+  updateEvent(eventDetails: EventDetails) {
     if (eventDetails.eventId) {
-      return this._firestore.doc(`events/${eventDetails.eventId}`).set(uploadData);
-    }
 
-    return this._firestore.collection('events').add(uploadData);
+      return this._firestore.doc(`events/${eventDetails.eventId}`).set({
+        accomodation: eventDetails.accomodation,
+        accomodationPrice: eventDetails.accomodationPrice,
+        description: eventDetails.eventDescription,
+        eventName: eventDetails.eventName,
+        eventMainPhotoUrl: eventDetails.eventMainPhotoUrl,
+
+        eventPhotosUrl: eventDetails.eventPhotosUrl,
+
+        eventPrice: eventDetails.eventPrice,
+        mapLat: eventDetails.mapCenter.lat,
+        mapLng: eventDetails.mapCenter.lng,
+
+        organizerId: eventDetails.organizerId,
+        organizerName: eventDetails.organizerName,
+
+        transport: eventDetails.transport,
+        transportPrice: eventDetails.transportPrice,
+      });
+    }
   }
 }
